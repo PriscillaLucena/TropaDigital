6e4528f39f0eb784e583bebfb520d26bdafb2957
PriscillaLucena <prilucenaoliveira@gmail.com>
2023-02-06 12:41:33 -0300

    finalizado dnd

--- tropa-digital/src/App.js
+++ tropa-digital/src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Router from "./Routes/Router";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+
 
 
 function App() {
   return (
     <div>
-      <DndProvider backend={HTML5Backend}>
-        <Router />
-      </DndProvider>
+      <Router />
     </div>
   );
 }
--- tropa-digital/src/Pages/LoginPage.js
+++ tropa-digital/src/Pages/LoginPage.js
@@ -1,37 +1,43 @@
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { goToHomePage } from "../Routes/RouterFunctions";
 import logo from "../images/logoTropa.png";
 import ceu from "../images/ceuEstrelado.png";
 import pc from "../images/pc.png";
 import senha from "../images/senha.png";
+import Draggable, { DraggableCore } from "react-draggable";
 import {
     ContainerButton, ContainerCard, ContainerGeral, ContainerImagemPc, ContainerLetras,
     ContainerPrancheta, ContainerRetangulo3, ContainerBox1, ContainerBox2, Laranja
 } from "../PageStyles/LoginStyles";
-import { onDragOver, onDragStart, onDrop } from "../dnd/dndFunctions";
 
 
 export const LoginPage = () => {
 
     const navigate = useNavigate()
 
-
+    const eventHandler = (e, data) => {
+        console.log('Event Type', e.type);
+        console.log({ e, data });
+    };
 
     return (
         <div>
-
-            <div id={'dragabble'} draggable="true" ondragstart={(e) => onDragStart(e.target)}>
-                <ContainerCard></ContainerCard>
-                <ContainerLetras>Bem Vindo ao <Laranja>painel</Laranja></ContainerLetras>
-                <ContainerRetangulo3 src={ceu} ></ContainerRetangulo3>
-                <ContainerPrancheta src={logo}></ContainerPrancheta>
-                <ContainerImagemPc src={pc}></ContainerImagemPc>
-                <ContainerBox1>Digite seu e-mail </ContainerBox1>
-                <ContainerBox2>Digite sua senha
-                    <img src={senha} /> </ContainerBox2>
-                <ContainerButton onClick={() => goToHomePage(navigate)} >Acessar</ContainerButton>
-            </div>
-            <ContainerGeral ondragover={(e) => onDragOver(e.target)} ondrop={(e) => onDrop(e.target)}></ContainerGeral>
+            <ContainerGeral>
+                <Draggable onMouseDown={eventHandler}>
+                    <div className="box">
+                        <ContainerCard></ContainerCard>
+                        <ContainerLetras>Bem Vindo ao <Laranja>painel</Laranja></ContainerLetras>
+                        <ContainerRetangulo3 src={ceu} ></ContainerRetangulo3>
+                        <ContainerPrancheta src={logo}></ContainerPrancheta>
+                        <ContainerImagemPc src={pc}></ContainerImagemPc>
+                        <ContainerBox1>Digite seu e-mail </ContainerBox1>
+                        <ContainerBox2>Digite sua senha
+                            <img src={senha} /> </ContainerBox2>
+                        <ContainerButton onClick={() => goToHomePage(navigate)} >Acessar</ContainerButton>
+                    </div>
+                </Draggable>
+            </ContainerGeral>
 
         </div>
     )
